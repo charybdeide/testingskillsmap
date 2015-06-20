3bf7cbb94d371aff347cc86c89ec17917e5cdf0b
Alexandra <alexandra.casapu@softhouse.se>
2015-06-20 20:30:19 +0300

    adding all new files from the moved folder

---
+++ testingskillsmap/public/js/main.js
@@ -0,0 +1,42 @@
+$(function(){
+	$("#aboutBtn").click(function()
+	{
+		$(".aboutContent").removeClass("hide");
+    	$(".description").addClass("hide");
+	});
+	$("#startBtn").click(function()
+	{
+		$(".makeMapContent").removeClass("hide");
+    	$(".description").addClass("hide");
+	});
+	$('[data-toggle="tooltip"]').tooltip({
+    container : 'body'
+  	});
+  	$("#addCategory").click(function()
+  	{
+  		$(".skillsMap").append($("#categoryTemplate").html());
+  		$(".category:last-of-type").append($("#initialSkillsTemplate").html());
+  	});
+
+  	$(".skillsMap").on("click", ".addSkill", function(event) {
+  		$(this).parent().children("ul").append($("#extraSkillTemplate").html());
+  	});
+
+	$(".skillsMap").on("click", ".removeSkill", function(event) {
+  		$(this).parent().remove();  		
+  	});
+  	
+	$(".skillsMap").on("click", ".removeCategory", function(event) {
+  		$(this).parent().remove();  		
+  	});
+
+  	$("#mySection").on("click", "#downloadBtn", function(event) {
+  		
+  		var list = [];
+  		var list = yourMap.check($(".skillsMap"));
+  		console.log(JSON.stringify(list));
+  		
+  	});
+
+  	
+});
