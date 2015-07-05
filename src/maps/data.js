ae1c7ff27705df1cabeb724c7f8f16704ae73dce
Alexandra <alexandra.casapu@softhouse.se>
2015-07-05 18:41:19 +0300

    added newly created files

---
+++ src/maps/data.js
@@ -0,0 +1,16 @@
+function getSkills(mapData) {
+	var list = [];
+	var mapLength = mapData.length;
+        for(var i = 0; i < mapLength; i++)
+        {
+          var skillsNumber = mapData[i].skills.length;
+            for(var j = 0; j < skillsNumber; j++)
+              if(mapData[i].skills[j] != '')
+                list.push(mapData[i].skills[j]);
+        }
+    return list;
+}
+
+module.exports = {
+  getSkills: getSkills
+};
---
+++ tests/keywordsTests.js
@@ -0,0 +1,19 @@
+'use strict';
+var assert = require("assert");
+var map = require("../src/maps/data.js");
+
+describe('Map data operations', function () {
+
+	it('should return empty list when no skills are stored', function() {
+		var result = map.getSkills([{category: "cat1", skills: ""}, {category: "cat2", skills: ""}]);
+		assert.equal(0, result.length);
+	})
+
+	it('should return the list of skills from all categories from the map', function() {
+		var result = map.getSkills([{category: "cat1", skills: ["cat1-s1", "cat1-s2", "cat1-s3"]}, {category: "cat2", skills: ["cat2-s1", "cat2-s2", "cat2-s3"]}]);
+		var list = ["cat1-s1", "cat1-s2", "cat1-s3", "cat2-s1", "cat2-s2", "cat2-s3"];
+		assert.deepEqual(list, result);
+	})
+
+});
+
