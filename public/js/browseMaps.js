f8a841c5e1653d9c27a279550b5509eb30fa26d0
Alexandra <alexandra.casapu@softhouse.se>
2015-08-22 17:56:17 +0300

    placed circlies on the middle of the line

--- public/js/browseMaps.js
+++ public/js/browseMaps.js
@@ -19,23 +19,36 @@ $(function() {
 			var availableWidth = $(this).width() - 
 								 $(this).find(".published-map-category:first").outerWidth() -
 								 $(this).find(".published-map-category:last").outerWidth();
-			console.log($(this).width());
-			console.log(availableWidth);
-			console.log( $(this).find(".published-map-category:first").outerWidth());
-			console.log($(this).find(".published-map-category:last").outerWidth());
+		
 			var colSize = availableWidth / (circles - 2);
 			
+			var emptySpace = $(this).width() - totalWidth;
 			//arrange the circles
-			$(this).find(".published-map-category").each(function(i) {
-				var w = $(this).outerWidth();
-				
-				var top = height - $(this).height() / 2;
-				var size = i === 0 || i === circles - 1 ? 0 : (colSize - w) / 2;
+			if(emptySpace < 0)
+			{
+				$(this).find(".published-map-category").each(function (i) {
+					var w = $(this).outerWidth();
 
-				$(this).css({top: top, left: left + size});
-				
-				left += i === 0 ? w : colSize;
-			});
+					var top = height - $(this).height() / 2;
+					var size = i === 0 || i === circles - 1 ? 0 : (colSize - w) / 2;
+
+					$(this).css({ top: top, left: left + size });
+
+					left += i === 0 ? w : colSize;
+				});
+			}
+			else 
+			{
+				var marginSpace = emptySpace/2;
+				left += marginSpace;
+				$(this).find(".published-map-category").each(function (i) {
+					var w = $(this).outerWidth();
+					var top = height - $(this).height() / 2;
+					$(this).css({ top: top, left: left });
+					left += w;
+				});
+			}
+	
 		});
 	}).resize();
 });
