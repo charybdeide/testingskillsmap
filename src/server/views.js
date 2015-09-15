5872cc6b2d5fd7c3bf367caa5e23ef2c18c8dfa3
Szabo Bogdan <szabobogdan@yahoo.com>
2015-09-15 12:04:32 +0300

    fixed view

--- src/server/views.js
+++ src/server/views.js
@@ -7,37 +7,46 @@ var defaultContext = {
 
 handlebars.registerHelper('login', function(template) {
   var session = template.data.root.session;
+  var out;
   if(!session || !session.user)
   {
-    var out = "<a href='/login'>Login</a>";  
+    out = "<a href='/login'>Login</a>";
   }
   else
+  {
     out = session.name + " <a href='/logout'>Logout</a>";
-  
-  return out; 
-})
+  }
+
+  return out;
+});
 
 handlebars.registerHelper('mainButton', function(template) {
   var session = template.data.root.session;
+  var out;
+
   if(!session || !session.user)
   {
-    var out = "<a href='/login' class='btn btn-lg btn-default'>Login to start</a>";
+    out = "<a href='/login' class='btn btn-lg btn-default'>Login to start</a>";
   }
-  else
+  else {
     out = "<a href='/create' class='btn btn-lg btn-default'>Work on your map</a>";
+  }
   return out;
-})
+});
 
 handlebars.registerHelper('leftMenuButtons', function(template) {
   var session = template.data.root.session;
+  var out;
+
   if(!session || !session.user)
   {
-    var out = "<li><a href='/about'>About</a></li>";
+    out = "<li><a href='/about'>About</a></li>";
   }
-  else
+  else {
     out = "<li><a href='/about'>About</a></li><li><a href='/create'>Create</a></li><li><a href='/browse'>Browse</a></li>";
+  }
   return out;
-})
+});
 
 var init = function(server, path) {
 
@@ -61,10 +70,13 @@ var init = function(server, path) {
 function validateUser(request) {
   return new Promise(function(resolve, reject) {
     var session = request.session.get('session');
-    if (!(session && session.user)) reject(new Error("Unauthorized"));
-    else resolve(session);  
+    if (!(session && session.user)) {
+      reject(new Error("Unauthorized"));
+    } else {
+      resolve(session);
+    }
   });
-  
+
 }
 
 module.exports =  {
