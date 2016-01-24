36d034bd5e537933325c1f7581389a950b262f8c
Szabo Bogdan <szabobogdan@yahoo.com>
2016-01-24 14:12:39 +0200

    Log resources

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,4 +1,7 @@
 var log = require('../log.js');
+var logResource = require('../logResource.js');
+var oldUptime = 0;
+var oldConcurents = 0;
 
 function GoodReporterExample (events, config) {
 
@@ -11,6 +14,19 @@ GoodReporterExample.prototype.init = function (readstream, emitter, callback) {
   log('Init hapi logger');
 
   readstream.on('data', function(data) {
+
+    if(data.event === "ops") {
+
+      var concurents = data.load.concurrents["3000"];
+
+      if(data.proc.uptime - oldUptime > 10 || concurents !== oldConcurents) {
+        var memory = (data.proc.mem.rss / data.os.mem.total) * 100;
+        logResource(data.proc.uptime + "," + memory + "," + concurents);
+
+        oldUptime = data.proc.uptime;
+        oldConcurents = concurents;
+      }
+    }
 
     if(data.event === "response") {
       log(data.method,
